019071174c5a03b9abd5e1f5fcd65a748d26791d
howard-bitgaming <157110201+howard-bitgaming@users.noreply.github.com>
2024-02-08 04:12:12 +0000

    restore

--- index.mjs
+++ index.mjs
@@ -1,14 +1,9 @@
  import * as core from '@actions/core'
  import * as github from  '@actions/github';
  import fs from 'node:fs'
- import checkout from 'checkout/dist/index.js'
  
 try {
-  console.log(checkout)
-const files = fs.readdirSync('./') 
-console.log('current',files)
-const files2 = fs.readdirSync('../') 
-console.log('prev',files2)
+
    
   const repository = core.getInput('repository');
   const file = core.getInput('file');
