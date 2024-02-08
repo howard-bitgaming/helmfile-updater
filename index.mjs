627c7ef2211e22533b31142850fbfac78b2cee13
howard-bitgaming <157110201+howard-bitgaming@users.noreply.github.com>
2024-02-08 04:21:57 +0000

    node

--- index.mjs
+++ index.mjs
@@ -1,26 +1,32 @@
- import * as core from '@actions/core'
- import * as github from  '@actions/github';
- import fs from 'node:fs'
- 
-try {
+import * as core from '@actions/core'
+import * as github from '@actions/github';
+import fs from 'node:fs'
+import { execSync } from 'node:child_process'
 
-   
+
+try {
   const repository = core.getInput('repository');
+  execSync(`git clone ${github.context.payload.repository.owner}/${repository} ../`)
+  console.log(checkout.getSource)
+  const files = fs.readdirSync('./')
+  console.log('current', files)
+  const files2 = fs.readdirSync('../')
+  console.log('prev', files2)
   const file = core.getInput('file');
   const branch = core.getInput('branch');
   const key = core.getInput('key');
   const value = core.getInput('value');
 
- //clone 目標專案
- //切branch
- //更該資料
- //commit push
- 
-console.log('repository', repository)
-console.log('file', file)
-console.log('branch', branch)
-console.log('key', key)
-console.log('value', value)
+  //clone 目標專案
+  //切branch
+  //更該資料
+  //commit push
+
+  console.log('repository', repository)
+  console.log('file', file)
+  console.log('branch', branch)
+  console.log('key', key)
+  console.log('value', value)
 
 
 } catch (error) {
