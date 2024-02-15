4d181a0a5e417318130b9263bdd36e27ec668b13
howard <howard@bitgaming.biz>
2024-02-15 11:48:20 +0800

    node

--- index.mjs
+++ index.mjs
@@ -1,5 +1,8 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github';
+import * as exec from '@actions/exec';
+import * as io from '@actions/io';
+import * as toolCache from '@actions/tool-cache';
 import fs from 'node:fs'
 import { execSync } from 'node:child_process'
 
@@ -10,13 +13,25 @@ try {
   const branch = core.getInput('branch');
   const key = core.getInput('key');
   const value = core.getInput('value');
-  const token = core.getInput('token')
-  const login = github.context.payload.repository.owner.login
+  const token = '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' || core.getInput('token')
+  const login = 'howard-bitgaming' || github.context.payload.repository.owner.login
+  const octokit = new github.getOctokit(token)
+  octokit.rest.repos.downloadZipballArchive({
+    owner: login,
+    repo: 'action-test-helmfile',
+    ref: 'beta',
+  }).then(resp => {
+    fs.appendFile('../a.zip', Buffer.from(resp.data), function (err) {
+      if (err) {
+        console.log('err', err)
+      } else {
+        toolCache.extractZip('../a','../')
+      }
+    });
+  })
+  // execSync(`git clone https://${token}@github.com/${login}/${repository}.git -b ${branch} ../${repository}`)
 
-  execSync(`git clone https://${token}@github.com/${login}/${repository}.git -b ${branch} ../${repository}`)
 
-  const files2 = fs.readdirSync('../')
-  console.log('prev', files2)
 
 
   //clone 目標專案
