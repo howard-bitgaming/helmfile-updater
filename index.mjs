0164307e6c1c1f946f354980ece1f61781496a0f
howard <howard@bitgaming.biz>
2024-02-19 11:24:31 +0800

    ff

--- index.mjs
+++ index.mjs
@@ -20,7 +20,18 @@ try {
   set(key, value)
   const targetYamlStr = yamljs.stringify(target, 2)
   fs.writeFileSync(file,targetYamlStr)
-
+  io.which('git').then(git=>{
+    exec.exec(git,['config', 'user.name', 'github-actions']).then(()=>{
+      return exec.exec(git,['config', 'user.email', 'github-actions@github.com'])
+    }).then(()=>{
+      return exec.exec(git,['add', '.'])
+    }).then(()=>{
+      return exec.exec(git,['commit', '-m',`set ${key} to ${value}`])
+    }).then(()=>{
+      return exec.exec(git,['push'])
+    })
+  })
+  
 } catch (error) {
   core.setFailed(error.message);
 }
