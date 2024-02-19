3016f5c44f77ec1ac65437b5421bb1e128b01cc5
howard <howard@bitgaming.biz>
2024-02-19 13:37:21 +0800

    dd

--- index.mjs
+++ index.mjs
@@ -7,7 +7,7 @@ import fs from 'node:fs'
 import path from 'node:path'
 import { execSync } from 'node:child_process'
 import { set } from 'object-selectors';
-import yamljs from 'yamljs';
+import yaml from 'yaml';
 
 const token = core.getInput('token')
 const octokit = new github.getOctokit(token)
@@ -16,11 +16,12 @@ const key = core.getInput('key');
 const value = core.getInput('value');
 
 try {
-  const target = yamljs.load(file)
+  const _file = fs.readFileSync(file, 'utf8')
+  const target = yaml.parse(_file)
   core.debug(`object ${JSON.stringify(target)}`)
   core.debug(`set ${key} to ${value}`)
   set(key,target, value)
-  const targetYamlStr = yamljs.stringify(target, 2)
+  const targetYamlStr = yaml.stringify(target, 2)
   fs.writeFileSync(file,targetYamlStr)
   io.which('git').then(git=>{
     exec.exec(git,['config', 'user.name', 'github-actions']).then(()=>{
