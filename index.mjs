2939e6864b495232f5a4a43e755fd554f26df1eb
howard <howard@bitgaming.biz>
2024-02-19 11:39:48 +0800

    dd

--- index.mjs
+++ index.mjs
@@ -17,6 +17,7 @@ const value = core.getInput('value');
 
 try {
   const target = yamljs.load(file)
+  core.debug(`set ${key} to ${value}`)
   set(key, value)
   const targetYamlStr = yamljs.stringify(target, 2)
   fs.writeFileSync(file,targetYamlStr)
