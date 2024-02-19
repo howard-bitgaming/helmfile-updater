f570f1a704d14b989e7118409b7981d6fb47ef9e
howard <howard@bitgaming.biz>
2024-02-19 13:35:57 +0800

    node

---
+++ test/test.mjs
@@ -0,0 +1,13 @@
+import { set ,get} from 'object-selectors';
+import yaml from 'yaml';
+import fs from 'node:fs'
+
+const file = fs.readFileSync('./test/test.yml', 'utf8')
+const key = 'releases.*[name $= frontend].set.*[name === image\\.tag].value'
+const  value =  "12341"
+
+const target = yaml.parse(file)
+
+set(key,target,value)
+const targetYamlStr = yaml.stringify(target)
+fs.writeFileSync('./test/res.yaml',targetYamlStr)
